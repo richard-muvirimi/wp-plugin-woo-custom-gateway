db0caffbd9cfb38d5f4c14ad5944300d85ac2cdb
Richard Muvirimi <richard@tyganeutronics.com>
2025-12-26 21:55:29 +0200

    Update bump-version.php

--- bump-version.php
+++ bump-version.php
@@ -84,12 +84,12 @@ $files = [
         'replacement' => '${1}' . $newVersion,
         'description' => 'readme.txt stable tag'
     ],
-    // readme.md - Stable tag
+    // readme.md - First version mention in changelog
     [
         'file' => findFile($rootDir, 'readme.md'),
-        'pattern' => '/(\*\*_ Stable tag: _\*\* )\d+\.\d+\.\d+/',
-        'replacement' => '${1}' . $newVersion,
-        'description' => 'readme.md stable tag'
+        'pattern' => '/(## Changelog\s+\*\*_ )\d+\.\d+\.\d+( _\*\*)/',
+        'replacement' => '${1}' . $newVersion . '${2}',
+        'description' => 'readme.md changelog version'
     ],
 ];
 
